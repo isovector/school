7c35ccd1c5469501a1837ed5e890d5dce1c92711
Sandy Maguire <sandy@sandymaguire.me>
2015-03-22 14:56:10 -0400

    tree maybe works

---
+++ 4B/cs486/a5/tree.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Data.List (nub, group, sort, minimumBy, maximumBy)
+import Debug.Trace (trace)
+import Data.Ord (comparing)
+
+data Example = Ex [Float] Bool deriving (Show, Eq)
+type ExData = [Example]
+type Attr = Int
+
+data DecisionTree = Tree Attr Float DecisionTree DecisionTree
+                  | Classification Bool
+                  deriving Show
+
+wordsWhen :: (Char -> Bool) -> String -> [String]
+wordsWhen p s =  case dropWhile p s of
+                   "" -> []
+                   s' -> w : wordsWhen p s''
+                     where (w, s'') = break p s'
+
+parseExample :: String -> (String -> Bool) -> Example
+parseExample s p = Ex (map read $ init splits) (p $ last splits)
+  where splits = wordsWhen (== ',') s
+
+intDiv :: Int -> Int -> Float
+intDiv a b = fromIntegral a / fromIntegral b
+
+getAttr :: Attr -> Example -> Float
+getAttr a (Ex as _) = as !! a
+
+attrCmp :: Attr -> (Float -> Bool) -> Example -> Bool
+attrCmp a f e = f $ getAttr a e
+
+gain :: ExData -> Attr -> Float
+{-gain ed a = fromIntegral a -- entropy p total - remainder-}
+    {-where entropy p' total' = log2 p' total' - log2 n' total'-}
+            {-where n' = total' - p'-}
+
+          {-remainder = sum $ map rem' [0 .. length values - 1]-}
+          {-rem' i = (intDiv totali total) * entropy pi totali-}
+            {-where totali = length samples-}
+                  {-pi = length $ filter c12n samples-}
+                  {-v = values !! i-}
+                  {-samples = filter (attrCmp a (== v)) ed-}
+
+          {-log2 x y = -frac * logBase 2.0 frac-}
+            {-where frac = fromIntegral x / fromIntegral y-}
+          {-p = length $ filter c12n ed-}
+          {-total = length ed-}
+
+          {-values = nub $ map (getAttr a) ed-}
+
+-- classification of an example
+c12n :: Example -> Bool
+c12n (Ex _ c) = c
+
+mode :: Ord a => [a] -> a
+mode xs = head . maximumBy (comparing length) . group $ sort xs
+
+median :: Ord a => [a] -> a
+median xs = (sort xs) !! (div (length xs) 2)
+
+ttrace :: Show a => a -> a
+ttrace a = trace (show a) a
+
+dtl :: ExData -> [Attr] -> DecisionTree
+dtl [] _                           = Classification False
+dtl ed@(ex:_) as
+  | all (== c12n ex) $ map c12n ed = Classification $ c12n ex
+  | as == []                       = Classification . mode $ map c12n ed
+  | otherwise                      = Tree a thresh less more
+    where subtree :: (Float -> Float -> Bool) -> DecisionTree
+          subtree f = dtl (filter (attrCmp a $ f thresh) ed) as
+          less = subtree (<=)
+          more = subtree (>)
+          a = maximumBy (comparing $ gain ed) [0 .. length as]
+          thresh = median $ map (getAttr a) ed
+
+
+
+
+main = do s <- readFile "test-train.txt"
+          let ed = map (flip parseExample $ (== "colic.")) $ lines s
+          let as = [0]
+          print $ dtl ed as
+          -- putStrLn . unlines . map show $ map (gain ed) [0..15]
+          -- putStrLn . unlines $ map show ed
+
+
+
+{-In the decision tree, use only binary tests, i.e. each node should test whether-}
+{-a particular attribute has a value greater or smaller than a threshold. In-}
+{-deciding which attribute to test at any point, use the information gain metric.-}
+{-Set the node test threshold for each potential attribute using this same metric-}
+{-i.e. at each point, see all the values that exist for a particular attribute in-}
+{-the remaining instances, order those values, and try threshold values that are-}
+{-(half way) between successive attribute values. Use the threshold value that-}
+{-gives the highest information gain. Allow the same attribute to be tested again-}
+{-later in the tree (with a different threshold). This means that along a path-}
+{-from the root to a leaf, the same attribute might be tested multiple times.-}
+{-After learning the decision tree, use the horseTest file to test the-}
+{-generalization accuracy of the tree.-}
